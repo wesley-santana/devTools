f7d71c8accf66b61b2d0b4637bb57cc09ef26415
wesley-santana <wesleysantana_gabriel@outlook.com>
2021-04-26 22:08:21 -0300

    fix(novoCartao): typo error

--- js/novoCartao.js
+++ js/novoCartao.js
@@ -1,23 +1,23 @@
-(function(){
-	"use strict"
+(function () {
+  "use strict";
 
-	$(".novoCartao").submit(function(event){
+  $(".novoCartao").submit(function (event) {
+    var campoConteudo = $(".novoCartao-conteudo");
 
-		var campoConteudo = $(".novoCartao-contudo");
+    var conteudo = campoConteudo
+      .val()
+      .trim()
+      .replace(/\n/g, "<br>")
+      .replace(/\*\*([^\*][^\*]*)\*\*/g, "<b>$1</b>")
+      .replace(/\*([^\*]*)\*/g, "<em>$1</em>");
 
-		var conteudo = campoConteudo.val().trim()
-										  .replace(/\n/g, "<br>")
-										  .replace(/\*\*([^\*][^\*]*)\*\*/g, "<b>$1</b>")
-										  .replace(/\*([^\*]*)\*/g, "<em>$1</em>");
+    if (conteudo) {
+      controladorDeCartoes.adicionaCartao(conteudo);
+      $(document).trigger("precisaSincronizar");
+    }
 
-		if(conteudo){
-			controladorDeCartoes.adicionaCartao(conteudo);
-			$(document).trigger("precisaSincronizar");
-		}
+    campoConteudo.val("");
 
-		campoConteudo.val("");
-
-		event.preventDefault();
-
-	});
-})()
+    event.preventDefault();
+  });
+})();
